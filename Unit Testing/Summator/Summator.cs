157bf6c5786b3cdb790eafb5c6ba9b9a360c589c
Krum Asenov <86630761+Fu3lxx@users.noreply.github.com>
2022-05-09 17:57:05 +0300

    Add files via upload

---
+++ Unit Testing/Summator/Summator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public static class Summator
+    {
+
+        public static int Sum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                sum += arr[i];
+
+            }
+            return sum;
+        }
+
+        public static double Average(int[] arr)
+        {
+            if (arr.Length < 2)
+            {
+                throw new ArgumentException("There must be at least two numbers in array");
+            }
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+
+            }
+            return sum / arr.Length;
+
+
+        }
+    }
+}
